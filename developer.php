cddb49afb7a02e6c4ffd943fe789b7ad4802a98d
indieka900 <indiekaj@gmail.com>
2023-06-23 12:26:50 +0300

    PHP service desk

---
+++ developer.php
@@ -0,0 +1,123 @@
+<?php
+include("database2.php");
+
+$db= $conn;
+$tableName="complaints";
+$columns= ['ComplaintId', 'Location','Department','Description','Timestart','Status','Expert_assigned'];
+$fetchData = fetch_data($db, $tableName, $columns);
+$totalRows = fetc_total_rows($db, $tableName);
+$pending = fetc_pending_rows($db, $tableName);
+$complete = fetc_complete_rows($db, $tableName);
+
+//Fetch the all complaints from database
+function fetch_data($db, $tableName, $columns){
+   if(empty($db)){
+   $msg= "Database connection error";
+   }elseif (empty($columns) || !is_array($columns)) {
+   $msg="columns Name must be defined in an indexed array";
+   }elseif(empty($tableName)){
+   $msg= "Table Name is empty";
+   }else{
+
+   $columnName = implode(", ", $columns);
+   $query = "SELECT ".$columnName." FROM $tableName"." ORDER BY Timestart DESC"." LIMIT 3";
+   $result = $db->query($query);
+
+   if($result== true){ 
+   if ($result->num_rows > 0) {
+      $row= mysqli_fetch_all($result, MYSQLI_ASSOC);
+      $msg= $row;
+   } else {
+      $msg= "No Data Found"; 
+   }
+   }else{
+   $msg= mysqli_error($db);
+   }
+   }
+   return $msg;
+}
+
+
+//Fetch the total number of rows of complaints from database
+function fetc_total_rows($db, $tableName,){
+   if(empty($db)){
+      $msg= "Database connection error";
+   }elseif(empty($tableName)){
+      $msg= "Table Name is empty";
+   }else{
+      $sql = "SELECT COUNT(*) AS total_rows FROM $tableName";
+      $result1 = mysqli_query($db, $sql);
+      if ($result1) {
+            $row = mysqli_fetch_assoc($result1);
+            $totalRows = $row['total_rows'];
+
+            return $totalRows;
+      } else {
+            echo "Error: " . mysqli_error($db);
+      }
+   }
+}
+
+
+//fetch the number of rows of the completed/solved complaints
+function fetc_complete_rows($db, $tableName,){
+   if(empty($db)){
+      $msg= "Database connection error";
+   }elseif(empty($tableName)){
+      $msg= "Table Name is empty";
+   }else{
+      $sql = "SELECT COUNT(*) AS complete_rows FROM $tableName WHERE Status = 'Complete'";
+      $result1 = mysqli_query($db, $sql);
+   if ($result1) {
+      $row = mysqli_fetch_assoc($result1);
+      $complete = $row['complete_rows'];
+      return $complete;
+   } else {
+      echo "Error: " . mysqli_error($db);
+   }
+   }
+}
+
+
+//fetch the number of rows of the pending complaints
+function fetc_pending_rows($db, $tableName,){
+ if(empty($db)){
+  $msg= "Database connection error";
+ }elseif(empty($tableName)){
+   $msg= "Table Name is empty";
+}else{
+   $sql = "SELECT COUNT(*) AS pending_rows FROM $tableName WHERE Status = 'Pending'";
+   $result = $db->query($sql);
+   if ($result) {
+      $row = mysqli_fetch_assoc($result);
+      $pending = $row['pending_rows'];
+      return $pending;
+   } else {
+      echo "Error: " . mysqli_error($db);
+   }
+   }
+}
+
+
+//convert the date and time to relative time
+function convertToRelativeTime($datetime) {
+   $timestamp = strtotime($datetime);
+   $currentTimestamp = time();
+   $difference = $currentTimestamp - $timestamp;
+   if ($difference < 60) {
+       $timeAgo = $difference . " seconds ago";
+   } elseif ($difference < 3600) {
+       $minutes = floor($difference / 60);
+       $timeAgo = $minutes . " minute" . ($minutes > 1 ? "s" : "") . " ago";
+   } elseif ($difference < 86400) {
+       $hours = floor($difference / 3600);
+       $timeAgo = $hours . " hour" . ($hours > 1 ? "s" : "") . " ago";
+   } else {
+       $days = floor($difference / 86400);
+       $timeAgo = $days . " day" . ($days > 1 ? "s" : "") . " ago";
+   }
+
+   return $timeAgo;
+}
+
+?>
---
+++ service_desk.php
@@ -0,0 +1,203 @@
+<?php
+include("developer.php");
+?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="style1.css"> 
+     
+<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <title>ICT OFFICER</title> 
+</head>
+<body>
+    <nav>
+        <div class="logo-name">
+            <div class="logo-image">
+                <img src="images/logo.png" alt="">
+            </div>
+
+            <span class="logo_name">ICT OFFICER</span>
+        </div>
+
+        <div class="menu-items">
+            <ul class="nav-links">
+                <li><a href="#">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Dashboard</span>
+                </a></li>
+                <li><a href="complaint.php">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">Complaints</span>
+                </a></li>
+				<li><a href="#">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">solved complaints</span>
+                </a></li>
+				  <li><a href="#">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">Pending Complaints</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">Raise complaint</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">Transfer Complaints</span>
+			
+            </ul>
+            
+            <ul class="logout-mode">
+                <li><a href="#">
+                    <i class="uil uil-signout"></i>
+                    <span class="link-name">Logout</span>
+                </a></li>
+
+                <li class="mode">
+                    <a href="#">
+                        <i class="uil uil-moon"></i>
+                    <span class="link-name">Dark Mode</span>
+                </a>
+
+                <div class="mode-toggle">
+                  <span class="switch"></span>
+                </div>
+            </li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="dashboard">
+        <div class="top">
+            <i class="uil uil-bars sidebar-toggle"></i>
+
+            <div class="search-box">
+                <i class="uil uil-search"></i>
+                <input type="text" placeholder="Search here...">
+            </div>
+            
+            <img src="images/profile.png" alt="">
+        </div>
+
+        <div class="dash-content">
+            <div class="overview">
+                <div class="title">
+                    <i class="uil uil-tachometer-fast-alt"></i>
+                    <span class="text">Dashboard</span>
+                </div>
+
+                <div class="boxes">
+                    <div class="box box1">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="text">TOTAL COMPLAINTS</span>
+                        <span class="number">
+                            <?php 
+                                $number = $totalRows;
+                                echo $number;
+                            ?>
+                        </span>
+                    </div>
+                    <div class="box box2">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="text">SOLVED COMPLAINTS</span>
+                        <span class="number">
+                        <?php 
+                                $number = $complete;
+                                echo $number;
+                            ?>
+                        </span>
+                    </div>
+                    <div class="box box3">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="text">PENDING COMPLAINTS</span>
+                        <span class="number">
+                        <?php 
+                                $number = $pending;
+                                echo $number;
+                            ?>
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="activity">
+                <div class="title">
+                    <i class="uil uil-clock-three"></i>
+                    <span class="text">Recent Activity</span>
+                </div>
+
+                <div class="activity-data">
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>
+                                    <div class="data st">
+                                        <span >COMPLAINT ID</span>
+                                    </div>
+                                    
+                                </td>
+                                <td>
+                                    <div class="data st">
+                                        <span >DEPARTMENT</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="data st">
+                                        <span >LOCATION</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="data st">
+                                        <span >DURATION</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="data st">
+                                        <span >STATUS</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="data st">
+                                        <span >Expert Assigned</span>
+                                    </div>
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                            if(is_array($fetchData)){      
+                            $sn=1;
+                            foreach($fetchData as $data){
+                            ?>
+                            <tr >
+                            <td><?php echo $data['ComplaintId']??''; ?></td>
+                            <td><?php echo $data['Department']??''; ?></td>
+                            <td><?php echo $data['Location']??''; ?></td>
+                            <td><?php echo convertToRelativeTime($data['Timestart']); ?></td>
+                            <td><?php echo $data['Status']??''; ?></td>
+                            <td><?php echo $data['Expert_assigned']??''; ?></td>
+                            </tr>
+                            <?php
+                            $sn++;}}else{ ?>
+                            <tr>
+                                <td colspan="8">
+                            <?php echo $fetchData; ?>
+                            </td>
+                            <tr>
+                            <?php
+                            }?>
+                            </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="script.js"></script>
+</body>
+</html>
